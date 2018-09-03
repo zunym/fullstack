5f595598684f5057dcc4e2c98a476396ce5d4ee7
zunym <zunyinmon@zunyinmon.com>
2018-09-04 00:55:04 +0800

    fs

--- my-day4-formintegration/src/app/app.module.ts
+++ my-day4-formintegration/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
+import { MaterialModule } from './material.module';
+import { NoteComponent } from './components/note.component' //from new modules file that manually created
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
